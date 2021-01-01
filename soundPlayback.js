b59d2121f832cc759792bfe90ed45eab896b0767
Maya Kröger <maya.kroeger@haw-hamburg.de>
2021-01-01 15:49:16 +0100

    midi

--- soundPlayback.js
+++ soundPlayback.js
@@ -1,64 +1,59 @@
 let context = new AudioContext();
 
+function startSound() {
+    switch (x) {
+        case 1:
+            fetch("drumsounds/sound1.wav")
+            .then(response => response.arrayBuffer())
+            .then(undecodedAudio => context.decodeAudioData(undecodedAudio))
+            .then(audioBuffer => {
+                let sourceBufferNode = context.createBufferSource();
+                sourceBufferNode.buffer = audioBuffer;
 
-// Diese Daten bekommt man später von der Schnittstelle
-let flag1 = true;
-let flag2 = false;
-let flag3 = false;
-let flag4 = false;
+                sourceBufferNode.connect(context.destination);
+                sourceBufferNode.start(context.currentTime);
+            })
+            .catch(console.error);
+            break;
+        case 2:
+            fetch("drumsounds/sound2.wav")
+            .then(response => response.arrayBuffer())
+            .then(undecodedAudio => context.decodeAudioData(undecodedAudio))
+            .then(audioBuffer => {
+                let sourceBufferNode = context.createBufferSource();
+                sourceBufferNode.buffer = audioBuffer;
 
-if(flag1){
-    fetch("drumsounds/sound1.wav")
-    .then(response => response.arrayBuffer())
-    .then(undecodedAudio => context.decodeAudioData(undecodedAudio))
-    .then(audioBuffer => {
-        let sourceBufferNode = context.createBufferSource();
-        sourceBufferNode.buffer = audioBuffer;
+                sourceBufferNode.connect(context.destination);
+                sourceBufferNode.start(context.currentTime);
+            })
+            .catch(console.error);
+            break;
+        case 3:
+            fetch("drumsounds/sound3.wav")
+            .then(response => response.arrayBuffer())
+            .then(undecodedAudio => context.decodeAudioData(undecodedAudio))
+            .then(audioBuffer => {
+                let sourceBufferNode = context.createBufferSource();
+                sourceBufferNode.buffer = audioBuffer;
 
-        sourceBufferNode.connect(context.destination);
-        sourceBufferNode.start(context.currentTime);
-    })
-    .catch(console.error);
-}
+                sourceBufferNode.connect(context.destination);
+                sourceBufferNode.start(context.currentTime);
+            })
+            .catch(console.error);
+            break;
+        case 4:
+            fetch("drumsounds/sound4.wav")
+            .then(response => response.arrayBuffer())
+            .then(undecodedAudio => context.decodeAudioData(undecodedAudio))
+            .then(audioBuffer => {
+                let sourceBufferNode = context.createBufferSource();
+                sourceBufferNode.buffer = audioBuffer;
 
-if(flag2){
-    fetch("drumsounds/sound2.wav")
-    .then(response => response.arrayBuffer())
-    .then(undecodedAudio => context.decodeAudioData(undecodedAudio))
-    .then(audioBuffer => {
-        let sourceBufferNode = context.createBufferSource();
-        sourceBufferNode.buffer = audioBuffer;
+                sourceBufferNode.connect(context.destination);
+                sourceBufferNode.start(context.currentTime);
+            })
+            .catch(console.error);
+            break;
 
-        sourceBufferNode.connect(context.destination);
-        sourceBufferNode.start(context.currentTime);
-    })
-    .catch(console.error);
-}
-
-if(flag3){
-    fetch("drumsounds/sound3.wav")
-    .then(response => response.arrayBuffer())
-    .then(undecodedAudio => context.decodeAudioData(undecodedAudio))
-    .then(audioBuffer => {
-        let sourceBufferNode = context.createBufferSource();
-        sourceBufferNode.buffer = audioBuffer;
-
-        sourceBufferNode.connect(context.destination);
-        sourceBufferNode.start(context.currentTime);
-    })
-    .catch(console.error);
-}
-
-if(flag4){
-    fetch("drumsounds/sound4.wav")
-    .then(response => response.arrayBuffer())
-    .then(undecodedAudio => context.decodeAudioData(undecodedAudio))
-    .then(audioBuffer => {
-        let sourceBufferNode = context.createBufferSource();
-        sourceBufferNode.buffer = audioBuffer;
-
-        sourceBufferNode.connect(context.destination);
-        sourceBufferNode.start(context.currentTime);
-    })
-    .catch(console.error);
+    }
 }
