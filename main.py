7ed69080763af1895548b1aafdb6713469c27463
Fabian Braun <fabian.braun@haw-hamburg.de>
2021-01-11 11:50:33 +0100

    Dead zone fertig

--- main.py
+++ main.py
@@ -59,94 +59,175 @@ while True:
     ret, background = cap.read()
     background = cv2.flip(background, 1)
     
+    # Wird in jedem Fall ausgeführt. Dient nur dazu, den Code einklappen zu können.
+    if True:
+        # oben links
+        imageslice1 = background[0:cameraHeightHalf, 0:cameraWidthThird, :]
+        mask1 = createMask(imageslice1)
 
-    # oben links
-    imageslice1 = background[0:cameraHeightHalf, 0:cameraWidthThird, :]
-    mask1 = createMask(imageslice1)
+        #oben links 2
+        imageslice5 = background[0:cameraHeightThird, 0:cameraWidthThird+cameraWidthSixth, :]
+        mask5 = createMask(imageslice5)
 
-    contours, hierarchy = cv2.findContours(mask1, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    if len(contours) > 0:
-        contourLength = np.zeros(100, dtype=np.uint8)
-        for index in range(len(contours)):
-            contourLength[index] = cv2.contourArea(contours[index])
-        maxlength = np.max(contourLength)
-        if maxlength > csize:
+        contours, hierarchy = cv2.findContours(mask1, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        if len(contours) > 0:
+            contourLength = np.zeros(100, dtype=np.uint8)
+            for index in range(len(contours)):
+                contourLength[index] = cv2.contourArea(contours[index])
+            maxlength = np.max(contourLength)
+            if maxlength > csize:
 
-            if not nodeOnUpperLeft:
-                print('links oben')
-                nodeOnUpperLeftHasChanged = True
-            nodeOnUpperLeft = True
-        else:
-            nodeOnUpperLeft = False
+                if not nodeOnUpperLeft:
+                    print('links oben')
+                    nodeOnUpperLeftHasChanged = True
+                nodeOnUpperLeft = True
+            else:
+                nodeOnUpperLeft = False
 
-    # links unten
-    imageslice2 = background[cameraHeightHalf:cameraHeight, 0:cameraWidthThird, :]
-    mask2 = createMask(imageslice2)
+        contours, hierarchy = cv2.findContours(mask5, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        if len(contours) > 0:
+            contourLength = np.zeros(100, dtype=np.uint8)
+            for index in range(len(contours)):
+                contourLength[index] = cv2.contourArea(contours[index])
+            maxlength = np.max(contourLength)
+            if maxlength > csize:
 
-    contours, hierarchy = cv2.findContours(mask2, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    if len(contours) > 0:
-        contourLength = np.zeros(100, dtype=np.uint8)
-        for index in range(len(contours)):
-            contourLength[index] = cv2.contourArea(contours[index])
-        maxlength = np.max(contourLength)
-        if maxlength > csize:
+                if not nodeOnUpperLeft:
+                    print('links oben')
+                    nodeOnUpperLeftHasChanged = True
+                nodeOnUpperLeft = True
+            else:
+                nodeOnUpperLeft = False
 
-            if not nodeOnLowerLeft:
-                print('links unten')
-                nodeOnLowerLeftHasChanged = True
-            nodeOnLowerLeft = True
-        else:
-            nodeOnLowerLeft = False
+        # unten links
+        imageslice2 = background[cameraHeightHalf:cameraHeight, 0:cameraWidthThird, :]
+        mask2 = createMask(imageslice2)
 
-    # rechts oben
-    imageslice3 = background[0:cameraHeightHalf, cameraWidthHalf+cameraWidthSixth:cameraWidth, :]
-    mask3 = createMask(imageslice3)
+        # unten links 2
+        imageslice6 = background[cameraHeightHalf + cameraHeightSixth:cameraHeight, cameraWidthThird:cameraWidthHalf, :]
+        mask6 = createMask(imageslice6)
 
-    contours, hierarchy = cv2.findContours(mask3, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    if len(contours) > 0:
-        contourLength = np.zeros(100, dtype=np.uint8)
-        for index in range(len(contours)):
-            contourLength[index] = cv2.contourArea(contours[index])
-        maxlength = np.max(contourLength)
-        if maxlength > csize:
+        contours, hierarchy = cv2.findContours(mask2, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        if len(contours) > 0:
+            contourLength = np.zeros(100, dtype=np.uint8)
+            for index in range(len(contours)):
+                contourLength[index] = cv2.contourArea(contours[index])
+            maxlength = np.max(contourLength)
+            if maxlength > csize:
 
-            if not nodeOnUpperRight:
-                print('rechts oben')
-                nodeOnUpperRightHasChanged = True
-            nodeOnUpperRight = True
-        else:
-            nodeOnUpperRight = False
+                if not nodeOnLowerLeft:
+                    print('links unten')
+                    nodeOnLowerLeftHasChanged = True
+                nodeOnLowerLeft = True
+            else:
+                nodeOnLowerLeft = False
 
-    # rechts unten
-    imageslice4 = background[cameraHeightHalf:cameraHeight, cameraWidthHalf+cameraWidthSixth:cameraWidth, :]
-    mask4 = createMask(imageslice4)
+        contours, hierarchy = cv2.findContours(mask6, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        if len(contours) > 0:
+            contourLength = np.zeros(100, dtype=np.uint8)
+            for index in range(len(contours)):
+                contourLength[index] = cv2.contourArea(contours[index])
+            maxlength = np.max(contourLength)
+            if maxlength > csize:
 
-    contours, hierarchy = cv2.findContours(mask4, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    if len(contours) > 0:
-        contourLength = np.zeros(100, dtype=np.uint8)
-        for index in range(len(contours)):
-            contourLength[index] = cv2.contourArea(contours[index])
-        maxlength = np.max(contourLength)
-        if maxlength > csize:
-            if not nodeOnLowerRight:
-                print('rechts unten')
-                nodeOnLowerRightHasChanged = True
-            nodeOnLowerRight = True
-        else:
-            nodeOnLowerRight = False
+                if not nodeOnLowerLeft:
+                    print('links unten')
+                    nodeOnLowerLeftHasChanged = True
+                nodeOnLowerLeft = True
+            else:
+                nodeOnLowerLeft = False
+
+        # rechts oben
+        imageslice3 = background[0:cameraHeightHalf, cameraWidthHalf+cameraWidthSixth:cameraWidth, :]
+        mask3 = createMask(imageslice3)
+
+        # rechts oben 2
+        imageslice7 = background[0:cameraHeightThird, cameraWidthHalf:cameraWidthHalf+cameraWidthSixth, :]
+        mask7 = createMask(imageslice7)
+
+        contours, hierarchy = cv2.findContours(mask3, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        if len(contours) > 0:
+            contourLength = np.zeros(100, dtype=np.uint8)
+            for index in range(len(contours)):
+                contourLength[index] = cv2.contourArea(contours[index])
+            maxlength = np.max(contourLength)
+            if maxlength > csize:
+
+                if not nodeOnUpperRight:
+                    print('rechts oben')
+                    nodeOnUpperRightHasChanged = True
+                nodeOnUpperRight = True
+            else:
+                nodeOnUpperRight = False
+
+        contours, hierarchy = cv2.findContours(mask7, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        if len(contours) > 0:
+            contourLength = np.zeros(100, dtype=np.uint8)
+            for index in range(len(contours)):
+                contourLength[index] = cv2.contourArea(contours[index])
+            maxlength = np.max(contourLength)
+            if maxlength > csize:
+
+                if not nodeOnUpperRight:
+                    print('rechts oben')
+                    nodeOnUpperRightHasChanged = True
+                nodeOnUpperRight = True
+            else:
+                nodeOnUpperRight = False
+
+        # rechts unten
+        imageslice4 = background[cameraHeightHalf:cameraHeight, cameraWidthHalf+cameraWidthSixth:cameraWidth, :]
+        mask4 = createMask(imageslice4)
+
+        # rechts unten 2
+        imageslice8 = background[cameraHeightHalf+cameraHeightSixth:cameraHeight, cameraWidthHalf:cameraWidthHalf+cameraWidthSixth, :]
+        mask8 = createMask(imageslice8)
+
+        contours, hierarchy = cv2.findContours(mask4, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        if len(contours) > 0:
+            contourLength = np.zeros(100, dtype=np.uint8)
+            for index in range(len(contours)):
+                contourLength[index] = cv2.contourArea(contours[index])
+            maxlength = np.max(contourLength)
+            if maxlength > csize:
+                if not nodeOnLowerRight:
+                    print('rechts unten')
+                    nodeOnLowerRightHasChanged = True
+                nodeOnLowerRight = True
+            else:
+                nodeOnLowerRight = False
+
+        contours, hierarchy = cv2.findContours(mask8, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        if len(contours) > 0:
+            contourLength = np.zeros(100, dtype=np.uint8)
+            for index in range(len(contours)):
+                contourLength[index] = cv2.contourArea(contours[index])
+            maxlength = np.max(contourLength)
+            if maxlength > csize:
+                if not nodeOnLowerRight:
+                    print('rechts unten')
+                    nodeOnLowerRightHasChanged = True
+                nodeOnLowerRight = True
+            else:
+                nodeOnLowerRight = False
 
     # Select the region in the background where we want to add the image and add the images using cv2.addWeighted()
+
+    # oben links
     added_image1 = cv2.addWeighted(imageslice1, alpha, foreground1[0:cameraHeightHalf, 0:cameraWidthThird, :], 1 - alpha, 0)
-    added_image5 = cv2.addWeighted(background[0:cameraHeightThird, 0:cameraWidthThird+cameraWidthSixth, :], alpha, foreground1[0:cameraHeightThird, 0:cameraWidthThird+cameraWidthSixth, :], 1 - alpha, 0)
+    added_image5 = cv2.addWeighted(imageslice5, alpha, foreground1[0:cameraHeightThird, 0:cameraWidthThird+cameraWidthSixth, :], 1 - alpha, 0)
 
+    # unten links
     added_image2 = cv2.addWeighted(imageslice2, alpha, foreground2[cameraHeightHalf:cameraHeight, 0:cameraWidthThird, :], 1 - alpha, 0)
-    added_image6 = cv2.addWeighted(background[cameraHeightHalf + cameraHeightSixth:cameraHeight, cameraWidthThird:cameraWidthHalf, :], alpha, foreground2[cameraHeightHalf + cameraHeightSixth:cameraHeight, cameraWidthThird:cameraWidthHalf, :], 1 - alpha, 0)
+    added_image6 = cv2.addWeighted(imageslice6, alpha, foreground2[cameraHeightHalf + cameraHeightSixth:cameraHeight, cameraWidthThird:cameraWidthHalf, :], 1 - alpha, 0)
 
+    # oben rechts
     added_image3 = cv2.addWeighted(imageslice3, alpha, foreground3[0:cameraHeightHalf, cameraWidthHalf+cameraWidthSixth:cameraWidth, ], 1 - alpha, 0)
-    added_image7 = cv2.addWeighted(background[0:cameraHeightThird, cameraWidthHalf:cameraWidthHalf+cameraWidthSixth, :], alpha, foreground3[0:cameraHeightThird, cameraWidthHalf:cameraWidthHalf+cameraWidthSixth, :], 1 - alpha, 0)
+    added_image7 = cv2.addWeighted(imageslice7, alpha, foreground3[0:cameraHeightThird, cameraWidthHalf:cameraWidthHalf+cameraWidthSixth, :], 1 - alpha, 0)
 
+    # unten rechts
     added_image4 = cv2.addWeighted(imageslice4, alpha, foreground4[cameraHeightHalf:cameraHeight, cameraWidthHalf+cameraWidthSixth:cameraWidth, :], 1 - alpha, 0)
-    added_image8 = cv2.addWeighted(background[cameraHeightHalf+cameraHeightSixth:cameraHeight, cameraWidthHalf:cameraWidthHalf+cameraWidthSixth, :], alpha, foreground4[cameraHeightHalf+cameraHeightSixth:cameraHeight, cameraWidthHalf:cameraWidthHalf+cameraWidthSixth, :], 1 - alpha, 0)
+    added_image8 = cv2.addWeighted(imageslice8, alpha, foreground4[cameraHeightHalf+cameraHeightSixth:cameraHeight, cameraWidthHalf:cameraWidthHalf+cameraWidthSixth, :], 1 - alpha, 0)
     
 
     # Change the region with the result
